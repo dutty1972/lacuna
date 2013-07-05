16c84173f9771ab0b6b4554f6956ff48b77805e9
eXerigumo Clanjor (哆啦比猫) <cjxgm@126.com>
2013-07-05 18:31:43 +0800

    @ lpp-elm-ui-db-gen: now generate correct database

--- lpp-elm-ui-db-gen.pl
+++ lpp-elm-ui-db-gen.pl
@@ -1,24 +1,27 @@
 #!/usr/bin/perl
 
 my @ds = map {
-	my $d = $_;
-	if ($d =~ s/^-I//) { $d }
-	else { undef }
+	undef;
+	$1 if /^-I(.*(elementary|evas).*)$/;
 } split /\s+/, `pkg-config --cflags elementary`;
 
-my @db;
+my @funcs;
+#my @consts;
 for (@ds) {
-	print;
-	print" -> ";
-	/([a-z_]+)-[0-9]+$/ or next;
-	my $prefix = $1;
-	print "$prefix\n";
 	for (<$_/*.h>) {
-		open $F, '<', $_;
-		while (<$F>) { /[^_]((elm|evas)_[^()]+)\(.*/g and unshift @db, $1 }
+		open my $F, '<', $_ or die $!;
+		while (<$F>) {
+			/EAPI.*((elm|evas)_[^()\s]+)\s*\(.*/ and unshift @funcs, $1;
+			
+			# TODO: substitute for consts
+			# /#define\s+((ELM|EVAS)_[A-Z0-9_]+)/ and unshift @consts, $1;
+		}
 		close $F;
 	}
 }
 
-print(join("\n", sort(@db)));
+
+open my $F, '> lpp-elm-ui.func.db' or die $!;
+print $F join("\n", @funcs);
+close $F;
 
